41cdf66738181a9d50b0adac8370786aadcd4ff0
Salvador Servin <servin52@msn.com>
2024-01-07 09:40:16 -0800

    sort array

--- multiplynumberstogether.js
+++ multiplynumberstogether.js
@@ -1,19 +1,10 @@
-// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
 
-// Examples:
-// a = "xyaabbbccccdefww"
-// b = "xxxxyyyyabklmopq"
-// longest(a, b) -> "abcdefklmopqwxy"
-
-// a = "abcdefghijklmnopqrstuvwxyz"
-// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
-
-function longest(s1, s2) {
-  s3 = s1 + s2;
-  s4 = s3.split("");
-  s4 = s4.sort().filter(function (element, index, array) {
-    return element !== array[index - 1];
-  });
-
-  return s4.join("");
+function multiplyNumbersTogether(num) {
+  let product = 1;
+  for (let i = 0; i < num.length; i++) {
+    product *= num[i];
+  }
+  alert(product);
 }
+multiplyNumbersTogether([10, 20, 30]);
