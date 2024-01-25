7969f0bb97bbb1763c7eaced8e5a93907e7666ac
Salvador Servin <servin52@msn.com>
2024-01-25 14:50:02 -0800

    Count the divisor of a number

--- welcometocity.js
+++ welcometocity.js
@@ -1,19 +1,10 @@
-// Return the number (count) of vowels in the given string.
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
 
-// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// Example:
 
-// The input string will only consist of lower case letters and/or spaces.
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
 
-function getCount(str) {
-  var vowelsCount = 0;
-  var vowels = ["a", "e", "i", "o", "u"];
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < vowels.length; j++) {
-      if (str[i] === vowels[j]) {
-        vowelsCount++;
-      }
-    }
-  }
-
-  return vowelsCount;
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 }
