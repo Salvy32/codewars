b04a8a673fbfe1f7340eb3854dba7a0c415a03cf
Salvador Servin <servin52@msn.com>
2024-02-06 19:32:50 -0800

    remove anchor

---
+++ removeanchor.js
@@ -0,0 +1,12 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3);
+  }
+
+  return s.toFixed(2);
+}
