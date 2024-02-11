76b9742363a475ea055f36fc8af7936228094e11
Salvador Servin <servin52@msn.com>
2024-02-10 18:26:34 -0800

    summing a number digit

---
+++ summinganubersdigits.js
@@ -0,0 +1,26 @@
+// The Story:
+// Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+// Task Overview:
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
+
+function sumDigits(number) {
+  return Math.abs(number)
+    .toString()
+    .split("")
+    .reduce(function (a, b) {
+      return +a + +b;
+    }, 0);
+}
